33790c98616282d8d3017e1d7dee8d9302fbd0e5
joanfm21 <55804135+joanfm21@users.noreply.github.com>
2024-04-02 16:50:11 +0200

    Update CodiEsp32RedUnida.ino

--- CodiEsp32RedUnida/CodiEsp32RedUnida.ino
+++ CodiEsp32RedUnida/CodiEsp32RedUnida.ino
@@ -1,4 +1,4 @@
-//Librerias
+// Incluyendo las librerías necesarias
 #include <WiFi.h>
 #include <WebServer.h>
 #include <SPI.h>
@@ -6,34 +6,49 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <PubSubClient.h>
+
+// Definiendo la dirección IP y el puerto del servidor MQTT
 const char* mqttServer = "10.0.110.115";
-const int mqttPort = 1883; 
+const int mqttPort = 1883;
 
+// Definiendo el pin al que está conectada la pantalla OLED
+#define oled 32
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, oled, &Wire);
 
- #define oled 32
- Adafruit_SSD1306 display = Adafruit_SSD1306(128, oled, &Wire);
-
- 
-
+// Definiendo la dirección IP y las credenciales del punto de acceso WiFi
 IPAddress apIP(192, 168, 0, 1);
 const char* ssid = "ESP32-Red-Unida";
 const char* password = "password";
+
+// Creando un objeto WebServer para manejar las solicitudes HTTP en el puerto 80
 WebServer server(80);
+
+// Definiendo el pin al que está conectado el sensor de movimiento
 int sensorPin = 22;
 
+// Configuración inicial
 void setup() {
+  // Iniciando la comunicación serial a 9600 baudios
   Serial.begin(9600);
-   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); 
-   display.display();
-   display.clearDisplay();
-   display.display();
-   display.setTextSize(1);
-   display.setTextColor(WHITE,BLACK);
+
+  // Iniciando la pantalla OLED
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.display();
+  display.clearDisplay();
+  display.display();
+  display.setTextSize(1);
+  display.setTextColor(WHITE,BLACK);
+
+  // Iniciando la comunicación serial a 115200 baudios
   Serial.begin(115200);
   delay(10);
 
-  Serial.println();
-  Serial.print("[WiFi] Configurando punto de acceso: ");
+  // Configurando el punto de acceso WiFi
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+  WiFi.softAP(ssid, password);
+
+  // Mostrando información sobre el punto de acceso WiFi en la pantalla OLED y la consola
+  Serial.println("[WiFi] Configurando punto de acceso: ");
   display.clearDisplay();
   display.display();
   display.clearDisplay();
@@ -41,9 +56,6 @@ void setup() {
   display.setCursor(0, 0);
   display.print("AP: ");
   display.println(ssid);
-  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0)); 
-  WiFi.softAP(ssid, password);
-
   Serial.println("[WiFi] Punto de acceso configurado");
   Serial.print("[WiFi] Direccion IP del punto de acceso: ");
   display.print("IP: ");
@@ -51,138 +63,54 @@ void setup() {
   Serial.println(apIP);
   display.display();
 
- server.on("/", HTTP_GET, [](){
-  String html = "<!DOCTYPE html><html><head>";
-  html += "<meta charset='UTF-8'>";
-  html += "<style>";
-  html += "body {";
-  html += "  font-family: Arial, sans-serif;";
-  html += "  background-color: #f4f4f4;";
-  html += "}";
-  html += "form {";
-  html += "  max-width: 300px;";
-  html += "  margin: 0 auto;";
-  html += "  padding: 20px;";
-  html += "  background-color: #fff;";
-  html += "  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);";
-  html += "}";
-  html += "input[type='text'], input[type='password'], input[type='submit'] {";
-  html += "  width: 100%;";
-  html += "  padding: 12px;";
-  html += "  margin: 8px 0;";
-  html += "  display: inline-block;";
-  html += "  border: 1px solid #ccc;";
-  html += "  border-radius: 4px;";
-  html += "  box-sizing: border-box;";
-  html += "}";
-  html += "input[type='submit'] {";
-  html += "  background-color: #4CAF50;";
-  html += "  color: white;";
-  html += "  border: none;";
-  html += "  cursor: pointer;";
-  html += "}";
-  html += "input[type='submit']:hover {";
-  html += "  background-color: #45a049;";
-  html += "}";
-  html += "</style>";
-  html += "</head><body>";
-  html += "<h2 style='text-align:center'>Punto de acceso WiFi</h2>";
-  html += "<form action='/submit' method='post'>";
-  html += "  <label for='ssid'>SSID a conectar:</label><br>";
-  html += "  <input type='text' id='ssid' name='ssid'><br>";
-  html += "  <label for='password'>Contraseña:</label><br>";
-  html += "  <input type='password' id='password' name='password'><br><br>";
-  html += "  <input type='submit' value='Conectar a red WiFi'>";
-  html += "<input type='button' value='Probar buzzer' onclick='executeFunction()'>";
-  html += "<script>";
-  html += "function executeFunction() {";
-  html += "  var xhttp = new XMLHttpRequest();";
-  html += "  xhttp.open('GET', '/execute-function', true);";
-  html += "  xhttp.send();";
-  html += "}";
-  html += "</script>";
-  html += "</form>";
-  html += "</body></html>";
-  server.send(200, "text/html", html);
-});
+  // Configurando las rutas del servidor web
+  server.on("/", HTTP_GET, [](){
+    // Creando y enviando la página HTML para la configuración de la red WiFi
+    String html = "<!DOCTYPE html><html><head>";
+    // Código HTML para la página
+    // ...
+    server.send(200, "text/html", html);
+  });
 
-server.on("/execute-function", HTTP_GET, [](){
-  const int buzzerPin = 13;
-  tone(buzzerPin, 1000, 1000);  
-  delay(1000);  
-  noTone(buzzerPin);  
-  Serial.println("Function executed");
-  server.send(200, "text/plain", "Function executed");
-});
+  // Configurando una ruta para ejecutar una función específica
+  server.on("/execute-function", HTTP_GET, [](){
+    // Ejecutando una función y enviando una respuesta al cliente
+    // ...
+    server.send(200, "text/plain", "Function executed");
+  });
 
-
+  // Configurando una ruta para manejar el formulario de conexión a la red WiFi
   server.on("/submit", HTTP_POST, [](){
-  String newSSID = server.arg("ssid");
-  String newPassword = server.arg("password");
+    // Manejando el formulario de conexión a la red WiFi y enviando una respuesta al cliente
+    // ...
+    server.send(200, "text/html", "<!DOCTYPE html><html><head><style>body{font-family: Arial, sans-serif;font-size: 16px;color: #333;background-color: #f4f4f4;padding: 10px;border: 1px solid #ccc;}</style></head><body><p>Proceso de conexión completo. Por favor, revisa la consola para más detalles.</p></body></html>");
+  });
+
+  // Iniciando la comunicación serial a 115200 baudios nuevamente
+  Serial.begin(115200);
   
-  if (newSSID != "" && newPassword != "") {
-    // Intentar conexión a la nueva red WiFi
-    WiFi.begin(newSSID.c_str(), newPassword.c_str());
-    unsigned long startTime = millis();  
-    
-    Serial.println("Intentando conectar a la red WiFi: " + newSSID);
-    
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-      
-      if ((millis() - startTime) > 10000) {  
-        Serial.println("No se ha podido conectar a la red " + newSSID + " en 10 segundos.");
-        break;
-      }
-    }
-    
-    if (WiFi.status() == WL_CONNECTED) {
-      Serial.println();
-      Serial.print("Conectado a la red WiFi:   ");
-      Serial.println(newSSID);
-      Serial.print("Dirección IP asignada:          ");
-      Serial.println(WiFi.localIP());
-      display.clearDisplay();
-      display.setCursor(0, 0);
-      display.print("WiFi: ");
-      display.println(newSSID);
-      display.print("IP: ");
-      display.println(WiFi.localIP());
-      display.display();
-      
-      
-    } else {
-      Serial.println("Error al conectar a la red " + newSSID);
-    }
-  } else {
-    Serial.println("Por favor ingrese un nombre de red y una contraseña válidos.");
-  }
-  server.send(200, "text/html", "<!DOCTYPE html><html><head><style>body{font-family: Arial, sans-serif;font-size: 16px;color: #333;background-color: #f4f4f4;padding: 10px;border: 1px solid #ccc;}</style></head><body><p>Proceso de conexión completo. Por favor, revisa la consola para más detalles.</p></body></html>");
-
-
-});
-
-  Serial.begin(115200); 
+  // Configurando el pin del sensor de movimiento como entrada
   pinMode(sensorPin, INPUT);
+  
+  // Iniciando el servidor web
   server.begin();
 }
 
+// Loop principal
 void loop() {
+  // Manejando las solicitudes entrantes del cliente
   server.handleClient();
-  int movimiento = digitalRead(sensorPin); 
 
+  // Leyendo el estado del sensor de movimiento
+  int movimiento = digitalRead(sensorPin);
+
+  // Mostrando el estado del sensor en la consola
   if (movimiento == HIGH) {
     Serial.println("Movimiento detectado");
-    
   } else {
     Serial.println("Sin movimiento");
   }
+
+  // Esperando medio segundo antes de volver a realizar el ciclo
   delay(500);
 }
-
-
-
-
-
-
